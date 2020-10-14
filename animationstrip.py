695ab7f5df34bcafdc9d49afa9d25303be6c02b5
George Tokmaji <tokmajigeorge@gmail.com>
2020-10-15 01:54:32 +0200

    Use ArgumentParser and return OSError errno codes to the operating system

--- animationstrip.py
+++ animationstrip.py
@@ -19,9 +19,8 @@ import os
 import sys
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-def process(file : str):
+def process(file : str, output : str, overwrite : bool = False):
     reader = PdfFileReader(file)
-    parts = os.path.splitext(file)
     writer = PdfFileWriter()
     last = -1
 
@@ -34,11 +33,33 @@ def process(file : str):
     if last != lastPage:
         writer.addPage(reader.pages[-1])
 
-    with open(parts[0] + "_processed" + parts[1], "wb") as fobj:
+    with open(output, "wb" if overwrite else "xb") as fobj:
         writer.write(fobj)
         
 
 if __name__ == "__main__":
-    for i in range(1, len(sys.argv)):
-        print("Processing ", sys.argv[i]);
-        process(sys.argv[i])
+    import argparse
+    from errno import EEXIST, ENOENT
+    
+    parser = argparse.ArgumentParser(description="Strips LaTeX beamer animations from PDFs, leaving only the last slide per group.")
+    parser.add_argument("input", help="input file")
+    parser.add_argument("output", help="output file. if not specified, the input file gets overwritten", nargs="?")
+    parser.add_argument("-f", help="overwrite existing output files", action="store_true", dest="overwrite")
+    parser.add_argument("-v", help="verbose output", action="store_const", dest="log", const=lambda *args, **kwargs: print(*args, **kwargs), default=lambda *args, **kwargs: None)
+
+    result = parser.parse_args()
+    outputFile = result.output or result.input
+    result.log(result.input, "->", outputFile)
+    try:
+        process(result.input, outputFile, result.overwrite or not result.output)
+    except FileNotFoundError as e:
+        result.log("Error:", result.input, "not found!", file=sys.stderr)
+        sys.exit(ENOENT)
+    except FileExistsError:
+        result.log("Error:", outputFile, "already exists!", file=sys.stderr)
+        sys.exit(EEXIST)
+    except OSError as e:
+        result.log(e.strerror)
+        sys.exit(e.errno)
+
+    sys.exit(0)
